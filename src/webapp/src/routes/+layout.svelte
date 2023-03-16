56ee04adeb677b146a18739de8676f1ff0e52708
Yau Goh Chow <yaugoh.chow@gmail.com>
2023-03-17 07:31:51 +0800

    Initial layout and 3d scene

---
+++ src/webapp/src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import '../styles/styles.scss';
+</script>
+
+<div class="layout">
+	<div class="layout__header">Something</div>
+	<div class="layout__body">
+		<slot />
+	</div>
+	<div class="layout__footer">Somthing</div>
+</div>
+
+<style lang="scss">
+	.layout {
+		display: flex;
+		flex-direction: column;
+		height: 100%;
+
+		&__body {
+			flex: 1;
+		}
+	}
+</style>
--- src/webapp/src/routes/+page.svelte
+++ src/webapp/src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script lang="ts">
+	import Scene from '$lib/components/scene.svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Scene />
